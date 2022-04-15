00278c8a052cf83dea0fe6f5b17e5db64c552a17
Johnny <stormyjmaster@gmail.com>
2022-04-15 14:08:21 -0400

    fixes

--- classes/Database.php
+++ classes/Database.php
@@ -3,12 +3,23 @@
 class Database {
     private $mysqli;
 
+    
     public function __construct() {
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
         $this->mysqli = new mysqli(Config::$db["host"], 
+                Config::$db["user"], Config::$db["pass"], Config::$db["database"]);
+    }
+    
+    
+    /*
+    public function __construct() {
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+        $this->mysqli = new PDO(Config::$db["host"], 
                 Config::$db["user"], Config::$db["pass"]);
     }
-
+    */
+    
+    
     public function query($query, $bparam=null, ...$params) {
         $stmt = $this->mysqli->prepare($query);
 
@@ -25,4 +36,28 @@ class Database {
 
         return true;
     }
+    
+    /*
+    public function query($query, $bparam=null, ...$params) {
+        $stmt = $this->mysqli->prepare($query);
+
+        for($i = 1; $i <= count($params); $i++){
+            $stmt->bindParam($bparam, ...$params);
+        }
+
+        if (($res = $stmt->execute()) === false) {
+            return false;
+        }
+
+        if ($res !== false) {
+            if($res){
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            } else {
+                return "";
+            }
+        }
+
+        return true;
+    }
+    */
 }
