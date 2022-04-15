96ae702505cf4c8c2c2c1f317680a169f2ad21a3
Johnny <stormyjmaster@gmail.com>
2022-04-15 13:11:16 -0400

    fixes

--- classes/Config.php
+++ classes/Config.php
@@ -3,15 +3,14 @@
 class Config
 {
     public static $db = [
-        "host" => "localhost:3306",
-        "user" => "root",
-        "pass" => "password",
-        "database" => "netflix"
-
-        //"host" => "mysql:unix_socket=/cloudsql/master-vehicle-344423:us-east4:db-demo;dbname=netflix",
+        //"host" => "localhost:3306",
         //"user" => "root",
-        //"pass" => "fHC1tueuKA1a3xbJ",
-
+        //"pass" => "password",
         //"database" => "netflix"
+
+        "host" => "mysql:unix_socket=/cloudsql/master-vehicle-344423:us-east4:db-demo;dbname=netflix",
+        "user" => "root",
+        "pass" => "fHC1tueuKA1a3xbJ",
+        "database" => "netflix"
     ];
 }
--- classes/Database.php
+++ classes/Database.php
@@ -6,7 +6,7 @@ class Database {
     public function __construct() {
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
         $this->mysqli = new mysqli(Config::$db["host"], 
-                Config::$db["user"], Config::$db["pass"], Config::$db["database"]);
+                Config::$db["user"], Config::$db["pass"]);
     }
 
     public function query($query, $bparam=null, ...$params) {
